e3a101eb5227950a14bd671281d4a38b08bba055
Abhiroopks <56361306+Abhiroopks@users.noreply.github.com>
2025-03-09 13:12:19 -0400

    Setup Github Actions (#8)
    * add github action for pylint
    
    * add black, isort formatting and commit if pylint successful
    
    * make flask.yaml github actions neater
    
    * Fix python for pylint and minor docker changes
    
    * chore: auto format and lint passed
    
    * Add bandit security scan in CI
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- services/web/manage.py
+++ services/web/manage.py
@@ -1,13 +1,30 @@
+"""
+Manage a Flask-SQLAlchemy application.
+
+This script defines a Flask CLI group, which is a set of commands that can be
+executed from the command line using the `flask` command.
+
+The available commands are:
+
+*   ``create_db``: Create the database tables.
+
+"""
+
 from flask.cli import FlaskGroup
-
 from project import app, db
-
 
 cli = FlaskGroup(app)
 
 
 @cli.command("create_db")
 def create_db():
+    """
+    Create the database tables.
+
+    This command creates the database tables based on the models defined in the
+    application.
+
+    """
     db.create_all()
     db.session.commit()
 
